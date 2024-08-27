e38a40b6eaa529d28d53861957733a9d46016591
Kain <kainedezz@gmail.com>
2024-08-27 08:59:21 +0500

    created base bet-maker

---
+++ bet-maker/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/events')
+async def get_events():
+    pass
+
+
+@app.post('/bet')
+async def set_bet():
+    pass
+
+@app.get('/bet/{bet_id}')
+async def create_event():
+    pass
