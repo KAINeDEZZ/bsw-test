375884a87805f12bc4153cb465582b64bd14f811
Kain <kainedezz@gmail.com>
2024-08-28 18:35:29 +0500

    added entrypoint

---
+++ bet-maker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+
+if [ "$*" == "" ]; then
+  exec uvicorn app:app --host 0.0.0.0 --port 8000
+
+elif [ "$1" = "load_events" ]; then
+  exec python load_events.py
+
+elif [ "$1" = "upgrade" ]; then
+  exec alembic upgrade head
+
+elif [ "$1" = "revision" ]; then
+  exec alembic revision --autogenerate
+
+else
+  echo "Custom command:"
+  exec "$@"
+fi
